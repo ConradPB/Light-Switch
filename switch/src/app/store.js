057ab5640609729523b4f3bf28725b7c9d4a26ac
Conrad Philip Bugingo <cpbmbaz57@gmail.com>
2023-07-27 18:43:49 +0300

    updated app/completed store.js

--- switch/src/app/store.js
+++ switch/src/app/store.js
@@ -3,16 +3,56 @@ import ReactDOM from 'react-dom';
 import { configureStore } from '@reduxjs/toolkit';
 
 
-
-
-
-
-
-
+const toggle = () => {
+  return {type: 'toggle'} 
+}
+ 
+const initialState = 'off';
+const lightSwitchReducer = (state = initialState, action) => {
+  switch (action.type) {
+    case 'toggle':
+      return state === 'on' ? 'off' : 'on';
+    default:
+      return state; 
+  }
+} 
 
 
 export const store = configureStore({
-  reducer: {
-    
-  },
+  reducer: lightSwitchReducer
 });
+
+const render = () => {
+  ReactDOM.render(
+    <LightSwitch 
+      state={store.getState()}
+    />,
+    document.getElementById('root')
+  )
+}
+ 
+render(); // Execute once to render with the initial state.
+store.subscribe(render); // Re-render in response to state changes.
+
+// Receive the store's state as a prop.
+function LightSwitch(props) {
+  const state = props.state; 
+
+  // We adjust the UI based on the store's current state.
+  const bgColor = state === 'on' ? 'white' : 'black';
+  const textColor = state === 'on' ? 'black' : 'white';  
+ 
+  // The click handler dispatches an action to the store.
+  const handleLightSwitchClick = () => {
+    store.dispatch(toggle());
+  }
+ 
+  return (  
+    <div style={{background : bgColor, color: textColor}}>
+      <button onClick={handleLightSwitchClick}>
+        {state}
+      </button>
+    </div>
+  )
+}
+ 
